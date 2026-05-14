70e253eb76151730102682eb55e776740bdf1301
yesidagudelo-gif <yesidagudelo@cotecnova.edu.co>
2026-05-14 11:39:09 -0500

    Fix init function to use correct syntax

--- js/js.js
+++ js/js.js
@@ -2,4 +2,10 @@ let operandoa;
 let operandob;
 let operandoc;
 
-function init()
+function init(){
+  let resultado = document.getElementByid('resultado');
+  let reset = document.getElementByid('reset');
+  let suma = document.getElementByid('suma');
+  let resta = document.getElementByid('resta');
+  let multiplicacion = document.getElementByid('multiplicacion');
+  

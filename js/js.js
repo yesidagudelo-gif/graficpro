4628b6701d6907378b43707a77fc3afe8b692a41
yesidagudelo-gif <yesidagudelo@cotecnova.edu.co>
2026-05-14 11:41:48 -0500

    Fix getElementById calls in init function

--- js/js.js
+++ js/js.js
@@ -3,9 +3,22 @@ let operandob;
 let operandoc;
 
 function init(){
-  let resultado = document.getElementByid('resultado');
-  let reset = document.getElementByid('reset');
-  let suma = document.getElementByid('suma');
-  let resta = document.getElementByid('resta');
-  let multiplicacion = document.getElementByid('multiplicacion');
-  
+  let resultado = document.getElementById('resultado');
+  let reset = document.getElementById('reset');
+  let suma = document.getElementById('suma');
+  let resta = document.getElementById('resta');
+  let multiplicacion = document.getElementById('multiplicacion');
+  let division = document.getElementById('division');
+  let igual = document.getElementById('igual');
+  let punto = document.getElementById('punto');
+
+  let numero0 = document.getElementById('numero0');
+  let numero1 = document.getElementById('numero1');
+  let numero2 = document.getElementById('numero2');
+  let numero3 = document.getElementById('numero3');
+  let numero4 = document.getElementById('numero4');
+  let numero5 = document.getElementById('numero5');
+  let numero6 = document.getElementById('numero6');
+  let numero7 = document.getElementById('numero7');
+  let numero8 = document.getElementById('numero8');
+  let numero9 = document.getElementById('numero9');
